07ba4331affedbebeca596700a5fe6f4b22b4302
Vojtech <jezekvoj@gmail.com>
2023-03-12 14:29:46 +0000

    first commit

---
+++ JiraTestApp/JiraTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JiraTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Jira");
+        }
+    }
+}
